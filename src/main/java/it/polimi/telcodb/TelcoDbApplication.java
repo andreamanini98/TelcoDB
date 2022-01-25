c321fd7d6fc583d2570ee86ee5ac5dd42fcdc5a9
Andrea Manini <echo@MBP-di-Andrea.homenet.telecomitalia.it>
2022-01-25 16:38:31 +0100

    Initial commit

---
+++ src/main/java/it/polimi/telcodb/TelcoDbApplication.java
@@ -0,0 +1,13 @@
+package it.polimi.telcodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelcoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelcoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/polimi/telcodb/TelcoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package it.polimi.telcodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelcoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
