48f1c1ae2d6b00b83a330fa3f109362d7d28e183
andreamanini98 <andrea3.manini@mail.polimi.it>
2022-02-19 11:38:04 +0100

    Small fixes.

--- src/main/java/it/polimi/telcodb/entities/User.java
+++ src/main/java/it/polimi/telcodb/entities/User.java
@@ -24,7 +24,6 @@ public class User {
     public User() {
     }
 
-
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
